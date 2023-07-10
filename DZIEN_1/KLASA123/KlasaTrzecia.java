6e895c924f131c018739d37cb38c10bfe8ec3cf5
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 13:53:25 +0200

    Update KlasaTrzecia.java

--- DZIEN_1/KLASA123/KlasaTrzecia.java
+++ DZIEN_1/KLASA123/KlasaTrzecia.java
@@ -1,4 +1,4 @@
-public class KlasaTrzecia extends KlasaDruga {
+public class KlasaTrzecia extends KlasaDruga implements InterS {
     int d;
     public KlasaTrzecia(int a, int b, int c, int d) {
         super(a, b, c);
@@ -11,6 +11,20 @@ public class KlasaTrzecia extends KlasaDruga {
     public int suma() {
         return super.suma() + d;
     }
-    
-    
+
+
+    @Override
+    public void Opis() {
+        System.out.println("To jest implementacja intefeksu InterS w klasie KlasaTrzecia");
+    }
+
+    @Override
+    public int policz() {
+        return 444 + d;
+    }
+
+    @Override
+    public double wynik(int a, int b) {
+        return a/(2+b);
+    }
 }
