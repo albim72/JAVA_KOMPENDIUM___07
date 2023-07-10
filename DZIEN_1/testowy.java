6be73e52e5d7e4da1a2d8ef5b815eb0cab3728a6
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 10:53:48 +0200

    Create testowy.java

---
+++ DZIEN_1/testowy.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("to jest pierwszy program w języku Java");
+        System.out.println("sout -> skrót do System.out.println");
+
+        int a = 7;
+        System.out.println(a);
+        double b = 13.76;
+        System.out.println(b);
+        double c = b/a;
+        System.out.println(c);
+
+        int v = 8;
+        double k = (double) v/a;
+        System.out.println(k);
+
+        boolean g = true;
+        //System.out.println(g*2);
+
+
+        }
+    }
