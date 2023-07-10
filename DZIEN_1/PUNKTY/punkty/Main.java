f17e51ffa3a432ae62ef679a100589f43a3b0493
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 11:45:16 +0200

    Create  Main.java

---
+++ DZIEN_1/PUNKTY/punkty/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Punkty pkt = new Punkty();
+        pkt.ustawX(15);
+        pkt.ustawY(7);
+        System.out.println("wartość  x: " + pkt.pX());
+        System.out.println("wartość  y: " + pkt.pY());
+    }
+}
