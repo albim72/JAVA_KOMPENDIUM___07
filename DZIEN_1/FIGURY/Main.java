321cb0410c34d756c33b93f752fb994b66b6568e
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 15:00:27 +0200

    Update Main.java

--- DZIEN_1/FIGURY/Main.java
+++ DZIEN_1/FIGURY/Main.java
@@ -5,5 +5,18 @@ public class Main {
 
         Prostokat pr  = new Prostokat(4.6,8.8);
         System.out.println("pole prostokoąta = "  + pr.PoliczPole() + " cm2");
+
+        System.out.println("____________ Pole Trójkąta ______________");
+        Trojkat tr = new Trojkat(6.6,8.9);
+        System.out.println("pole trójkąta = "  + tr.PoliczPole() + " cm2");
+
+        System.out.println("____________ Pole Trapeza ______________");
+        Trapez trp = new Trapez(8.2,6.1,5.8);
+        System.out.println("pole trapezu = "  + trp.PoliczPole() + " cm2");
+
+        System.out.println("____________ Pole Koła ______________");
+        Kolo kl = new Kolo(5.5);
+        System.out.println("pole koła = "  + kl.PoliczPole() + " cm2");
+
     }
 }
