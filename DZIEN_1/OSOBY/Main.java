cb26a28a436a6abdd5fc89e955b6d17f42f61400
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 12:06:33 +0200

    Create Main.java

---
+++ DZIEN_1/OSOBY/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Osoba os1 = new Osoba("Aneta","Grech","Kraków",33,178.0,66.0);
+        os1.printOsoba();
+        Osoba os2 = new Osoba("Jan","Kot",56,180.0,99.0);
+        os2.printOsoba();
+        Osoba os3 = new Osoba();
+        os3.printOsoba();
+
+    }
+}
