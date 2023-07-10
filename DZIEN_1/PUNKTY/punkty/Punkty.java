1edddbd180c2fe53d070890b1d89543de35dcaff
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 11:41:48 +0200

    Create Punkty.java

---
+++ DZIEN_1/PUNKTY/punkty/Punkty.java
@@ -0,0 +1,11 @@
+public class Punkty {
+    private int wspX;
+    private int wspY;
+    
+    void ustawX(int x){wspX=x;}
+    //CTRL + D -> powielenie linii lub bloku kodu
+    void ustawY(int y){wspY=y;}
+    
+    int pX(){return wspX;}
+    int pY(){return wspY;}
+}
