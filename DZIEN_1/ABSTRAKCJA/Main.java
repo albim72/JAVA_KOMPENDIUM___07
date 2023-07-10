04a2876fa039868dce555e4ae50eb749c1c886b4
albim72 <marcin.albiniak72@gmail.com>
2023-07-10 14:11:14 +0200

    Create Main.java

---
+++ DZIEN_1/ABSTRAKCJA/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Regular reg = new Regular(7,8);
+        reg.info();
+
+        System.out.println(reg.policz());
+        System.out.println(reg.policzX());
+    }
+}
