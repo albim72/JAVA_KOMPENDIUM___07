afa5a3b1b6fd8cb6284853be5693bdfcf55dba2c
albim72 <marcin.albiniak72@gmail.com>
2023-07-11 09:16:35 +0200

    Create interfejs_funkcyjny.java

---
+++ DZIEN_2/interfejs_funkcyjny.java
@@ -0,0 +1,20 @@
+
+@FunctionalInterface
+interface Square{
+    int calculate(int x);
+}
+
+@FunctionalInterface
+interface Calc{
+    int calculate(int x, int y);
+}
+
+interface Big{
+    int calculate(int x, int y, int z);
+}
+
+public class Main {
+    public static void main(String[] args) {
+       
+    }
+}
