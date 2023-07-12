f80c7bf55f5bae4a2a102a95fa1a17bc347aa524
albim72 <marcin.albiniak72@gmail.com>
2023-07-12 10:12:50 +0200

    Create iterator.java

---
+++ DZIEN_3/iterator.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            al.add(i);
+        }
+        System.out.println(al);
+
+        Iterator<Integer> itr = al.iterator();
+
+        while (itr.hasNext()){
+            int i = itr.next();
+            System.out.print(i + " ");
+            if(i%2!=0)
+                itr.remove();
+
+        }
+        System.out.println();
+        System.out.println(al);
+    }
+}
