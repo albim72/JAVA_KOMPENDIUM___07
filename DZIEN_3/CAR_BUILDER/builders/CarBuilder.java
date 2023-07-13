597bcac11754ce96f0f17a45182cbb0ad437dae7
albim72 <marcin.albiniak72@gmail.com>
2023-07-13 08:51:08 +0200

    Create CarBuilder.java

---
+++ DZIEN_3/CAR_BUILDER/builders/CarBuilder.java
@@ -0,0 +1,53 @@
+package builders;
+
+import cars.CarType;
+import cars.Car;
+import components.Engine;
+import components.GPSNavigator;
+import components.Transmission;
+import components.TripComputer;
+public class CarBuilder implements Builder {
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    public CarBuilder() {
+    }
+
+    @Override
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setCTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+    
+    public Car getResult(){
+        return new Car(this.type,this.seats,this.engine,this.transmission,this.tripComputer,this.gpsNavigator);
+    }
+}
