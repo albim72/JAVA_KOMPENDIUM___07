c374102bdfb5d95a1d6d2e7d99e3075eeeceeae4
albim72 <marcin.albiniak72@gmail.com>
2023-07-14 13:14:48 +0200

    Create Consumer.java

---
+++ DZIEN_5/synchroQ/Consumer.java
@@ -0,0 +1,16 @@
+public class Consumer implements Runnable{
+    Q q;
+    Thread t;
+
+    public Consumer(Q q) {
+        this.q = q;
+        t = new Thread(this,"Konsument");
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            q.get();
+        }
+    }
+}
