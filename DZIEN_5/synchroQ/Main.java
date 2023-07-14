ea42144805904167e3b21a3c04b47ec4c16fb651
albim72 <marcin.albiniak72@gmail.com>
2023-07-14 13:17:50 +0200

    Create Main.java

---
+++ DZIEN_5/synchroQ/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+
+        p.t.start();
+        c.t.start();
+        System.out.println("Przerwij program żeby zakończyć!");
+
+    }
+}
