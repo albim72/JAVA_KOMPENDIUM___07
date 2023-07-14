e4e1b62dfd1ffe06a86f504425b3a655328c2dec
albim72 <marcin.albiniak72@gmail.com>
2023-07-14 13:28:02 +0200

    Create Check.java

---
+++ DZIEN_5/refelksja/Check.java
@@ -0,0 +1,8 @@
+public class Check {
+    private void privateMethod(){
+        System.out.println("Metoda prywatna -> wywołana na zewnątrz!");
+    }
+    public void printData(){
+        System.out.println("Metoda publiczna!");
+    }
+}
