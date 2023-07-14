09447c62721228e8fdf7f2d38dc3a24a27f4eab4
albim72 <marcin.albiniak72@gmail.com>
2023-07-14 09:24:36 +0200

    Create AudioMixer.java

---
+++ DZIEN_5/FASADA/some_comlex_media_lib/AudioMixer.java
@@ -0,0 +1,10 @@
+package some_complex_media_lib;
+
+import java.io.File;
+
+public class AudioMixer {
+    public File fix(VideoFile result){
+        System.out.printf("AdioMixer: fixing audio...");
+        return new File("tmp");
+    }
+}
